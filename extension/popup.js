bb6e7641833f1129c65e1df195e33cfcf0318901
Emile Amajar <74051072+emilamaj@users.noreply.github.com>
2024-03-02 23:56:17 +0100

    Add spinner + light styling

--- extension/popup.js
+++ extension/popup.js
@@ -2,6 +2,9 @@ document.getElementById('translateBtn').addEventListener('click', () => {
   const text = document.getElementById('textToTranslate').value;
   console.log('Text to translate:', text);
   if (text) {
+    const spinner = document.getElementById('spinnerContainer');
+    spinner.style.display = 'block'; // Show the spinner
+
     const translationEndpoint = 'http://localhost:5000/translate';
     fetch(translationEndpoint, {
       method: 'POST',
@@ -13,7 +16,11 @@ document.getElementById('translateBtn').addEventListener('click', () => {
       .then(response => response.text())
       .then(result => {
         document.getElementById('translationResult').textContent = result;
+        spinner.style.display = 'none'; // Hide the spinner
       })
-      .catch(error => console.log('Error:', error));
+      .catch(error => {
+        console.log('Error:', error);
+        spinner.style.display = 'none'; // Hide the spinner
+      });
   }
 });
