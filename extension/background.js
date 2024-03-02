5f19956f1a3155ddd12626936a5acf472fc99b10
Emile Amajar <74051072+emilamaj@users.noreply.github.com>
2024-03-02 18:57:41 +0100

    Display result to console

--- extension/background.js
+++ extension/background.js
@@ -1,26 +1,26 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-      id: "translate",
-      title: "Translate",
-      contexts: ["selection"]
-    });
+  chrome.contextMenus.create({
+    id: "translate",
+    title: "Translate",
+    contexts: ["selection"]
   });
-  
-  chrome.contextMenus.onClicked.addListener(function (info, tab) {
-    if (info.menuItemId === "translate" && info.selectionText) {
-      const translationEndpoint = process.env.TRANSLATION_ENDPOINT;
-      fetch(translationEndpoint, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ text: info.selectionText }),
-      })
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  if (info.menuItemId === "translate" && info.selectionText) {
+    console.log('Selected text:', info.selectionText);
+    const translationEndpoint = "http://localhost:5000/translate";
+    fetch(translationEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: info.selectionText }),
+    })
       .then(response => response.text())
       .then(result => {
-        // Display the result, you might want to change how it's displayed
-        alert(result);
+        console.log('Received translation:', result);
       })
       .catch(error => console.log('Error:', error));
-    }
-  });
+  }
+});
