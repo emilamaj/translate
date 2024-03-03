5b9d78a2127ecd6c8199322061a7bc33e5b22125
Emile Amajar <74051072+emilamaj@users.noreply.github.com>
2024-03-03 20:12:18 +0100

    todo: implement translation on context menu click

--- extension/background.js
+++ extension/background.js
@@ -12,20 +12,8 @@ chrome.contextMenus.onClicked.addListener(function (info, tab) {
   // Check if the clicked item is the "translate" menu item and if there is a selected text
   if (info.menuItemId === "translate" && info.selectionText) {
     console.log('Selected text:', info.selectionText);
-    const translationEndpoint = "http://localhost:5000/translate";
-    
-    // Send a POST request to the translation endpoint with the selected text
-    fetch(translationEndpoint, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ text: info.selectionText }),
-    })
-      .then(response => response.text())
-      .then(result => {
-        console.log('Received translation:', result);
-      })
-      .catch(error => console.log('Error:', error));
+
+    // Send a message to the content script to translate the selected text
+    // ...
   }
 });
