45b662311b91f616052e8d2e7ea99d51266d4c1e
Emile Amajar <74051072+emilamaj@users.noreply.github.com>
2024-03-27 10:07:06 +0100

    Remove context menu item (chrome api prevents opening popup from context menu)

--- extension/background.js
+++ extension/background.js
@@ -5,23 +5,3 @@ chrome.action.onClicked.addListener((tab) => {
       files: ['content.js']
   });
 });
-
-// Create a context menu item for translation when the extension is installed
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: "translate",
-    title: "Translate",
-    contexts: ["selection"]
-  });
-});
-
-// Listen for clicks on the context menu item
-chrome.contextMenus.onClicked.addListener(function (info, tab) {
-  // Check if the clicked item is the "translate" menu item and if there is a selected text
-  if (info.menuItemId === "translate" && info.selectionText) {
-    console.log('Selected text:', info.selectionText);
-
-    // Send a message to the content script to translate the selected text
-    // ...
-  }
-});
